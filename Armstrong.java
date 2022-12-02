b8abc2d7767c6172722e71f988a37d09fbe55b19
MoraXX <shubham.21dec03@gmail.com>
2022-12-03 01:02:11 +0530

    modified Armstrong.java

--- Armstrong.java
+++ Armstrong.java
@@ -2,10 +2,25 @@ import java.util.Scanner;
 
 public class Armstrong {
     public static void main(String[] args) {
-        int number,remainder,originalNumber,resuld=0;
+        int number,temp=0,remainder,sum=0;
         System.out.println("Enter your no");
         Scanner sc=new Scanner(System.in);
         number=sc.nextInt();
-
+        temp=number;
+        while(number>0)
+        {
+            remainder=number%10;
+            number=number/10;
+            sum=sum + remainder*remainder*remainder;
+        }
+        if(temp==sum)
+        {
+            System.out.println("The entered value is Armstrong a number");
+        }
+        else
+        {
+            System.out.println("The entered value is not Armstrong a number");
+        }
+        sc.close();
     }
 }
