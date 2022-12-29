85b86eecd849f6b56334e477e06da6ac873dc01d
MoraXX <shubham.21dec03@gmail.com>
2022-12-30 01:25:16 +0530

    Added heart pattern

--- pattern.java
+++ pattern.java
@@ -44,6 +44,33 @@ public class pattern {
         }
         System.out.println("\n\n");
     }
+    public static void pattern4() {
+        int a,b;
+        int size=15;
+        for(a=size/2;a<=size;a=a+2)
+        {
+            for(b=1;b<size-a;b=b+2)
+                System.out.print(" ");
+            for(b=1;b<=a;b++)
+                System.out.print("A");
+            for(b=1;b<=size-a;b++)
+                System.out.print(" ");            
+            for(b=1;b<=a-1;b++)
+                System.out.print("A");
+            
+        System.out.println();
+        }
+
+        for(a=size;a>=0;a--)
+        {
+            for(b=a;b<size;b++)
+                System.out.print(" ");
+            for(b=1;b<=((a*2)-1);b++)
+                System.out.print("B");
+
+        System.out.println();
+        }
+    }
     public static void main(String[] args) {
         int row,col;
        Scanner sc=new Scanner(System.in);
@@ -54,6 +81,7 @@ public class pattern {
         obj.pattern1(row,col);
         obj.pattern2(row,col);
         obj.pattern3(row,col);
+        obj.pattern4();
         sc.close();
     }
 }
